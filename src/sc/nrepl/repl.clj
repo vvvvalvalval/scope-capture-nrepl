5d86fd62802546adeff6b0d9d22500b11509d3ee
Rick Moynihan <rick.m@swirrl.com>
2019-01-04 18:01:19 +0000

    Prevent selecting an invalid exection point with sc.nrepl.repl/in-ep
    Resolves #1

--- src/sc/nrepl/repl.clj
+++ src/sc/nrepl/repl.clj
@@ -1,16 +1,18 @@
 (ns sc.nrepl.repl
-  (:require [sc.nrepl.impl :as i]))
+  (:require [sc.nrepl.impl :as i]
+            [sc.impl.db :refer [db]]))
 
 (defn in-ep
   "'In Execution Point' - Sets the current Execution Point Id to `ep-id`.
   Subsequent commands sent to the REPL via 'eval' or 'load-file'
   will be transformed so that each form `expr` is wrapped with (sc.api/letsc epid expr)"
   [ep-id]
-  (reset! i/current-ep-id ep-id)
+  (when (or (nil? ep-id)
+            (sc.impl/find-ep @db ep-id))
+    (reset! i/current-ep-id ep-id))
   nil)
 
 (defn exit
   "Exists the context of the current Execution Point, if any. Idempotent."
   []
   (in-ep nil))
-
