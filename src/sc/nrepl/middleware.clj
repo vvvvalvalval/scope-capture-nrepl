c0f03ced06e927aeb321821665752a151ea35879
Rick Moynihan <rick.m@swirrl.com>
2019-01-04 16:53:07 +0000

    Fixes #3 make compatible with nrepl 0.5.3 and leiningen 2.8.3

--- src/sc/nrepl/middleware.clj
+++ src/sc/nrepl/middleware.clj
@@ -1,7 +1,11 @@
 (ns sc.nrepl.middleware
-  (:require [clojure.tools.nrepl.middleware :as nrm]
-            [sc.nrepl.impl :as i]
+  (:require [sc.nrepl.impl :as i]
             [sc.nrepl.repl]))
+
+;; Support both new & legacy nrepl versions
+(if (find-ns 'nrepl.middleware)
+  (require '[nrepl.middleware :as nrm]) ;; newer nrepl 0.5.3 and lein 2.8.3+
+  (require '[clojure.tools.nrepl.middleware :as nrm])) ;; old tools.nrepl and earlier lein versions
 
 (defn wrap-letsc
   [handler]
